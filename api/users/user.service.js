fe9813be182d3c871626e5b1d91748a072283e26
AIS0001 <axialitsolutions0001@gmail.com>
2020-10-27 20:28:59 +0700

    user token modified

--- api/users/user.service.js
+++ api/users/user.service.js
@@ -2,6 +2,7 @@ const pool = require("../../config/database");
 
 module.exports = {
     create:(data,callback)=>{
+        var low =2000;
         var userid = Math.random() * (999 - low) + low
         pool.query(
             `INSERT INTO user_registration ( userid, password, cname, contact, city, address, pincode, date, empcode, refcode, cardno, payment_mode,type)
