32f26cbd3871ccbc8ce54904665d007d70b16932
AIS0001 <axialitsolutions0001@gmail.com>
2020-10-28 23:22:36 +0700

    dsf

--- api/admin/admin.router.js
+++ api/admin/admin.router.js
@@ -3,11 +3,11 @@ const router = require("express").Router();
 const { checkToken } = require("../../auth/token_validation");
 
 router.post("/create",createUser);
-router.post("/getusers",checkToken,getUsers);
-router.post("/getvendors",checkToken,getVendors);
+router.post("/getusers",getUsers);
+router.post("/getvendors",getVendors);
 router.post("/getemployees",checkToken,getEmployees);
 
-router.get("/updateemployee",checkToken,);
+//router.get("/updateemployee",checkToken,);
 router.get("/deleteemployee",checkToken,getUsers);
 
 
